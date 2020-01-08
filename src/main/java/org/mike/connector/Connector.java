426f4076181773f81ff1bb0ab9c93ed6f70a0f44
Mike Levitsky <levitskym@edi.su>
2020-01-08 11:03:49 +0200

    some refactor

--- src/main/java/org/mike/connector/Connector.java
+++ src/main/java/org/mike/connector/Connector.java
@@ -5,21 +5,85 @@ import org.apache.log4j.Logger;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.regex.Pattern;
 
 public class Connector {
 
     private static final Logger log = Logger.getLogger(Connector.class);
+    private final Ftpex ftpex;
 
-    public Connector(Config config) {
+    public Connector(final Config config) {
+        this.ftpex = new Ftpex(config);
         do {
-            log.info("start");
-            new Handler(config);
-            log.info("end");
+            try {
+                log.info("start");
+                ftpex.connect();
+
+                // inbound
+                for (final Folder folder : config.inboundFolders()) {
+                    final Map<String, byte[]> result = ftpex.getFiles(folder.serverPath, folder.doctype);
+                    if(result != null) {
+                        log.info("Got [" + result.size() + "] files for type [" + folder.doctype + "] from server [" + folder.serverPath + "]");
+                        writeResult(folder, result);
+                    }
+                }
+
+                // outbound
+                for (final Folder folder : config.outboundFolders()) {
+                    final Map<String, byte[]> files = prepareFiles(folder);
+                    log.info("Got [" + files.size() + "] files for type [" + folder.doctype + "] from local path [" + folder.localPath + "]");
+                    ftpex.uploadFiles(folder.localPath, folder.serverPath, files);
+                }
+
+                log.info("end");
+            } catch (Exception e){
+                e.printStackTrace();
+                log.error(e);
+            } finally {
+                ftpex.close();
+            }
             sleep(config.intervalValue());
         } while (config.isDaemon());
+    }
+
+    private void writeResult(final Folder folder, final Map<String, byte[]> result) {
+        for (final String name : result.keySet()) {
+            try {
+                final Path path = Paths.get(folder.localPath);
+                if(!Files.exists(path))
+                    Files.createDirectories(path);
+
+                Files.write(Paths.get(folder.localPath).resolve(name), result.get(name));
+                log.info("file ["+name+"] saved to ["+folder.localPath+"]");
+                ftpex.removeFile(name);
+            } catch (Exception e) {
+                log.error(e);
+            }
+        }
+    }
+
+    private Map<String, byte[]> prepareFiles(final Folder folder) {
+        final Map<String, byte[]>files = new HashMap<>();
+        File dir = new File(folder.localPath);
+        for (String name : Objects.requireNonNull(dir.list((dir1, name1) -> Pattern.matches(folder.doctype, name1)))) {
+            try {
+                files.put(name.replaceAll("[а-яА-ЯёЁ ]", ""), Files.readAllBytes(Paths.get(folder.localPath).resolve(name)));
+                log.info("file [" + name + "] extracted from [" + folder.localPath + "]");
+            } catch (IOException e) {
+                log.error(e);
+            }
+        }
+        return files;
     }
 
     public static void main(String[] args) throws Exception {
@@ -27,7 +91,7 @@ public class Connector {
         new Connector(initConfig(configFile));
     }
 
-    private static Config initConfig(String path) throws JAXBException, FileNotFoundException {
+    private static Config initConfig(final String path) throws JAXBException, FileNotFoundException {
         Unmarshaller um = JAXBContext.newInstance(Config.class).createUnmarshaller();
         return (Config)um.unmarshal(new FileInputStream(Paths.get(path).toString()));
     }
--- src/main/java/org/mike/connector/Ftpex.java
+++ src/main/java/org/mike/connector/Ftpex.java
@@ -4,6 +4,9 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.ConnectException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
@@ -17,128 +20,23 @@ public class Ftpex {
 
 	private static final Logger log = Logger.getLogger(Ftpex.class);
 
+	private static final int MAX_RECONNECT_ATTEMPTS = 5;
+
 	private Config conf;
 	private FTPClient client;
 
-	public Ftpex(Config conf) {
+	public Ftpex(final Config conf) {
 		this.conf = conf;
-        connect();
 	}
 
-	public Map<String, byte[]> getFiles(String folder, String filter) {
-        return getFiles(folder, filter, true);
-	}
-
-    private Map<String, byte[]> getFiles(String folder, String filter, boolean reconnect) {
-        final Map<String, byte[]> files = new HashMap<>();
-        try{
-            checkConnection();
-            changeSrvFolder(folder);
-            for (String name : this.client.listNames()) {
-                if(Pattern.matches(filter, name)) {
-                    byte[]file = getFile(name);
-                    if(file != null) {
-                        files.put(name, file);
-                    }
-                }
-            }
-        } catch (Exception e) {
-            if(reconnect){
-                log.info("Connection lost.. try to reconnect ..");
-                reConnect();
-                return getFiles(folder, filter, false);
-            } else {
-                log.error(e);
-            }
-        }
-        return files;
+    void connect() throws Exception {
+	    this.connect(1);
     }
 
-    public void uploadFiles(String folder, Map<String,byte[]> files) {
-        uploadFiles(folder, files, true);
-    }
-
-	private void uploadFiles(String folder, Map<String,byte[]> files, boolean reconnect) {
+    void connect(int attemptCounter) throws Exception {
         try {
-            checkConnection();
-            changeSrvFolder(folder);
-            for (final String name : files.keySet()) {
-                uploadFile(name, files.get(name));
-            }
-        } catch (Exception e) {
-            if(reconnect){
-                log.info("Connection lost.. try to reconnect ..");
-                reConnect();
-                uploadFiles(folder, files, false);
-            } else {
-                log.error(e);
-            }
-        }
-	}
-	
-	private byte[] getFile(String fileName) {
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		try {
-			this.client.setFileType(FTP.BINARY_FILE_TYPE);
-			this.client.retrieveFile(fileName, baos);
-			if(baos.size() != 0) {
-				log.info("file ["+fileName+"] downloaded from "+this.client.getRemoteAddress()+"::"+this.conf.login+"::"+this.client.printWorkingDirectory());
-				return baos.toByteArray();
-			}
-		} catch (IOException e) {
-			log.error(e);
-		}
-		return null;
-	}
-
-	public void removeFile(String fileName) {
-		try {
-			if(this.client.deleteFile(fileName))
-                log.info("file ["+fileName+"] removed from "+this.client.getRemoteAddress()+"::"+this.conf.login+"::"+this.client.printWorkingDirectory());
-		} catch (IOException e) {
-			log.error(e);
-		}
-	}
-
-	private void uploadFile(String fileName, byte[]file) throws Exception {
-		ByteArrayInputStream bais = new ByteArrayInputStream(file);
-        this.client.setFileType(FTP.BINARY_FILE_TYPE);
-        if(this.client.storeFile(fileName, bais))
-            log.info("file ["+fileName+"] uploaded to "+this.client.getRemoteAddress()+"::"+this.conf.login+"::"+this.client.printWorkingDirectory());
-	}
-	
-	private void changeSrvFolder(String folder) throws Exception {
-        while(!this.client.printWorkingDirectory().equals("/"))
-            this.client.changeToParentDirectory();
-        this.client.changeWorkingDirectory(folder);
-	}
-
-    public void close() {
-        try {
-            if(this.client != null)
-                this.client.disconnect();
-        } catch (Exception e){
-            log.error(e);
-        }
-    }
-
-    private void reConnect() {
-        try {
-            close();
-            connect();
-        } catch (Exception e){
-            log.error(e);
-        }
-    }
-
-    public void checkConnection() {
-        if(this.client == null || !this.client.isConnected())
-            connect();
-    }
-
-    public void connect(){
-        try {
-            this.client = new FTPClient();
+            if(this.client == null)
+                this.client = new FTPClient();
 
             if(!this.client.isConnected()) {
                 this.client.connect(this.conf.server);
@@ -151,9 +49,89 @@ public class Ftpex {
             if(conf.debug){
                 this.client.addProtocolCommandListener(new PrintCommandListener(new PrintWriter(System.out)));
             }
+        } catch (ConnectException e){
+            if(attemptCounter >= MAX_RECONNECT_ATTEMPTS)
+                throw new Exception("Maximum reconnects exceeded");
+            log.warn("Connection lost.. try to reconnect ["+attemptCounter+"] ..");
+            this.connect(++attemptCounter);
+        }
+    }
+
+    void close() {
+        try {
+            if(this.client != null)
+                this.client.disconnect();
         } catch (Exception e){
             log.error(e);
         }
     }
+
+    Map<String, byte[]> getFiles(final String folder, final String filter) throws Exception {
+        final Map<String, byte[]> files = new HashMap<>();
+        connect();
+        changeSrvFolder(folder);
+        for (final String name : this.client.listNames()) {
+            if(Pattern.matches(filter, name)) {
+                final byte[]file = getFile(name);
+                if(file != null) {
+                    files.put(name, file);
+                }
+            }
+        }
+        return files;
+	}
+
+    void uploadFiles(final String localFolder, final String srvFolder, final Map<String,byte[]> files) throws Exception {
+        connect();
+        changeSrvFolder(srvFolder);
+        for (final String name : files.keySet()) {
+            uploadFile(name, files.get(name));
+            removeLocalFile(name, localFolder);
+        }
+    }
+
+    private void removeLocalFile(final String name, final String path) {
+        try {
+            Files.delete(Paths.get(path).resolve(name));
+            log.info("file ["+name+"] removed from ["+path+"] O.K.");
+        } catch (IOException e) {
+            log.error(e);
+        }
+    }
+	
+	private byte[] getFile(final String fileName) throws Exception {
+        connect();
+        try(final ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+            this.client.setFileType(FTP.BINARY_FILE_TYPE);
+            this.client.retrieveFile(fileName, baos);
+            if(baos.size() != 0) {
+                log.info("file ["+fileName+"] downloaded from "+this.client.getRemoteAddress()+"::"+this.conf.login+"::"+this.client.printWorkingDirectory());
+                return baos.toByteArray();
+            }
+        }
+        return null;
+	}
+
+	void removeFile(final String fileName) throws Exception {
+        connect();
+        if(this.client.deleteFile(fileName))
+            log.info("file ["+fileName+"] removed from "+this.client.getRemoteAddress()+"::"+this.conf.login+"::"+this.client.printWorkingDirectory());
+	}
+
+	private void uploadFile(final String fileName, final byte[]file) throws Exception {
+        connect();
+        try(final ByteArrayInputStream bais = new ByteArrayInputStream(file)){
+            this.client.setFileType(FTP.BINARY_FILE_TYPE);
+            if(this.client.storeFile(fileName, bais))
+                log.info("file ["+fileName+"] uploaded to "+this.client.getRemoteAddress()+"::"+this.conf.login+"::"+this.client.printWorkingDirectory());
+        }
+	}
+	
+	private void changeSrvFolder(final String folder) throws Exception {
+        connect();
+        while(!this.client.printWorkingDirectory().equals("/"))
+            this.client.changeToParentDirectory();
+        this.client.changeWorkingDirectory(folder);
+	}
 
 }
